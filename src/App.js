d025ebc7f3d202120b2975fa4bcb07658d36a1eb
yamuna-FSD-Developer <yamunag1812@gmail.com>
2024-01-31 13:47:12 +0530

    reactapp

--- src/App.js
+++ src/App.js
@@ -2,51 +2,107 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from './Card';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-function App() {
+
+function App(){
   return (
-    <div className="container">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        <Card planName="Free" price="0" users=" Single User" storage="50GB Storage" project="Unlimited Public Projects" access="Community Access" project2="Unlimited Private Projects" support="Dedicated Phone Support" sub="Free Subdomain" report="Monthly Status Report"/>
-        <Card planName="Plus" price="9" users="5 Users" storage="50GB Storage" project="Unlimited Public Projects" access="Community Access" project2="Unlimited Private Projects" support="Dedicated Phone Support" sub="Free Subdomain" report="Monthly Status Report"/>
-        <Card planName="Pro" price="49" users="Unlimited Users" storage="50GB Storage" project="Unlimited Public Projects" access="Community Access" project2="Unlimited Private Projects" support="Dedicated Phone Support" sub="Free Subdomain" report="Monthly Status Report"/>
-        <h1>Hi</h1>
-     <i className="bi-alarm"></i>
+    <div id='root'>
+       <div>
+          <h1 style={{textAlign:"center"}}> REACT PRICE TAG</h1>
+        </div>
+    
+      <div class="container">
+            <div class="row bg-primary">
+               <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4 mt-5 ml-2 mr-2-4">
+                 <div className="card border mb-4 box-shadow">
+                    <div className="card-header text-center"><h3> FREE</h3>
+        <h2>
+          $ 0/month
+        </h2>
+        </div>
+        <div className='card-body'>
+        <ul className="list-unstyled mt-3 mb-4">
+          <li> <i className="bi bi-check"></i>Single user</li>
+          <li> <i className="bi bi-check"></i>50GB Storage</li>
+          <li> <i className="bi bi-check"></i>Community Access</li>
+          <li className='text-muted'> <i className="bi bi-x"></i>Unlimited private projects</li>
+          <li className='text-muted'> <i className="bi bi-x"></i>Dedicated phone Support</li>
+          <li className='text-muted'> <i className="bi bi-x"></i>free subdomain</li>
+          <li className='text-muted'> <i className="bi bi-x"></i>Monthly status Reports</li>
+        </ul>
+
         <button
         type='button'
-        className="w-100 btn btn-danger">BUTTON</button>
+        className="w-100 btn btn-lg btn-primary" disabled>BUTTON</button>
+
+      </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4 mt-5 ml-2 mr-2">
+      <div className="card border mb-4 box-shadow">
+        <div className='card-header text-center'><h3> PLUS</h3>
+        <h2>
+          $ 9/month
+        </h2>
+        </div>
+        <div className='card-body'>
+        <ul className="list-unstyled mt-3 mb-4">
+          <li> <i className="bi bi-check"></i>Single user</li>
+          <li> <i className="bi bi-check"></i>50GB Storage</li>
+          <li> <i className="bi bi-check"></i>Community Access</li>
+          <li> <i className="bi bi-check"></i>Unlimited private projects</li>
+          <li> <i className="bi bi-check"></i>Dedicated phone Support</li>
+          <li> <i className="bi bi-check"></i>free subdomain</li>
+          <li className='text-muted'> <i className="bi bi-x"></i>Monthly status Reports</li>
+        </ul>
+
+        <button
+        type='button'
+        className=" w-100 btn btn-lg btn-primary "disabled>BUTTON</button>
+
+        </div>
+        </div>
+        </div>
+        
+
+        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-4 mt-5 ml-2 mr-2-4">
+           <div className="card border">
+        <div className='card-header text-center'><h3> PRO</h3>
+        <h2>
+          $ 49/month
+        </h2>
+        </div>
+        <div className='card-body'>
+        <ul className="list-unstyled mt-3 mb-4">
+          <li> <i className="bi bi-check"></i>Single user</li>
+          <li> <i className="bi bi-check"></i>50GB Storage</li>
+          <li> <i className="bi bi-check"></i>Community Access</li>
+          <li> <i className="bi bi-check"></i>Unlimited private projects</li>
+          <li> <i className="bi bi-check"></i>Dedicated phone Support</li>
+          <li> <i className="bi bi-check"></i>free subdomain</li>
+          <li className="bi bi-check">Monthly status Reports</li>
+        </ul>
+
+        <button
+        type='button'
+        className=" w-100 btn btn-lg btn-primary ">BUTTON</button>
+
+        </div>
+        </div>
+        </div>
+      
+      
+        
+     
           </div>
           </div>
-
-
-
-
-      
-
-
-
-
-    
-      
-    
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+          </div>
+          
+          
+  )
 }
 
 export default App;
+
+
+
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,31 +1,28 @@
 function Card(props){
-    return <div class="col">
-    <div class="card">
-       
-         <h4 class="my-0 fw-normal">{props.planName}</h4>
-         </div>
-          <div class="card-body">
-      <h1 class="card-title
-      pricing-card-title">
-        ${props.price}<small
-        class="text-boby-secondary
-        fw-light">/mo</small>
-        </h1>
-        <ul class="list-unstyled mt-3 mb-4">
-          <li> <i className="bi-alarm"></i>{props.users}</li>
-          <li>{props.storage}</li>
-          <li>{props.project}</li>
-          <li>{props.access}</li>
-          <li>{props.project2}</li>
-          <li>{props.support}</li>
-          <li>{props.sub}</li>
-          <li>{props.report}</li>
-        </ul>
-        <button
-        type='button'
-        class="w-100 btn btn-danger">BUTTON</button>
-    </div>
-    </div>
+    return <div class="col-4">
+    <div class="card mb-4 box-shadow">
+     <div class="card-header bg-white text-center m-md-2" style="color: gray;">
+         <h4 class="my-0 font-weight-normal">FREE</h4></div>
+        <div class="card-body">
+      <h1 class="card-title pricing-card-title">$0<small class="text-muted">/month</small></h1>
+   <ul class="list-unstyled mt-3 mb-4"><li><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg> Single User</li>
+   <li><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg> 50GB Storage</li>
+   
+   <li><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg> Unlimited Public Projects</li>
+   
+   <li><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg> Community Access</li>
+   
+   <li style="color: lightgrey;"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" class="svg-inline--fa fa-xmark " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path></svg>  Unlimited Private Projects</li>
+   
+   <li style="color: lightgrey;"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" class="svg-inline--fa fa-xmark " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path></svg>  Dedicated Phone Support</li>
+   
+   <li style="color: lightgrey;"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" class="svg-inline--fa fa-xmark " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path></svg>  Free Subdomain</li>
+   
+   <li style="color: lightgrey;"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" class="svg-inline--fa fa-xmark " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path></svg>  Monthly Status Reports</li></ul><button type="button" class="button button2">BUTTON</button>
+   </div>
+   </div>
+   </div>
+          
 }
 
 export default Card;
